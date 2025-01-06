44ceacc385a86b51e8a84edd39acfc1745aa7509
Korab <korab.morina44@gmail.com>
2025-01-06 18:03:21 +0200

    send motivational quotes on Mondays via Email

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import smtplib
+import datetime as dt
+import random
 
 my_email = "korabmtest@gmail.com"
 password = "htjgwqutatyiknog"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="korab.morina444@gmail.com",
-        msg="Subject:Hello\n\nThis is the body of my email.")
+now = dt.datetime.now()
+current_day_week = now.weekday()
+if current_day_week == 0:
+    with open("quotes.txt") as data:
+        text = data.readlines()
+        quote = random.choice(text)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+                from_addr=my_email,
+                to_addrs="korab.morina444@gmail.com",
+                msg=f"Subject:Motivation quote\n\n{quote}")
+
+
+
+

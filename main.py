8423ea695f98f93a099418ed7817ee164decd0cc
Korab <korab.morina44@gmail.com>
2025-01-28 21:16:27 +0200

    send motivational quotes on Mondays via Email

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+import pandas
 import smtplib
 import datetime as dt
 import random
 
-my_email = "korabmtest@gmail.com"
-password = "htjgwqutatyiknog"
+MY_EMAIL = "korabmtest@gmail.com"
+PSW = "htjgwqutatyiknog"
 
+data = pandas.read_csv("birthdays.csv")
 now = dt.datetime.now()
-current_day_week = now.weekday()
-if current_day_week == 0:
-    with open("quotes.txt") as data:
-        text = data.readlines()
-        quote = random.choice(text)
+today_tuple = (now.month, now.day)
+print(today_tuple)
+
+birthday_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
-        connection.login(user=my_email, password=password)
+        connection.login(MY_EMAIL, PSW)
         connection.sendmail(
-                from_addr=my_email,
-                to_addrs="korab.morina444@gmail.com",
-                msg=f"Subject:Motivation quote\n\n{quote}")
-
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person.email,
+            msg=f"Subject: Happy Birthday!\n\n {contents}")
 
 
 
